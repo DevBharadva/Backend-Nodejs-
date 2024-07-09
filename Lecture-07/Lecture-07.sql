878b476832c649d1d19bcbfbd742e6b05562e175
Dev Bharadva <150213586+DevBharadva@users.noreply.github.com>
2024-07-09 10:01:00 +0530

    Update Lecture-07.sql

--- Lecture-07/Lecture-07.sql
+++ Lecture-07/Lecture-07.sql
@@ -1,17 +1,9 @@
--- Primary key 
--- Primary key is a Uniquely idenetifes each row in a table
--- Primary key e Unique key banave che je ek jevi biji key jenrate kartu nathi
-
-
--- FOREIGN  KEY
--- FOREIGN  KEY is a create link between two tables by referancing the primary key of another table
-
-
-
-
-
 create database Shop;
 
+show databases;
+use Shop;
+
+drop  table client_master;
 create table CLIENT_MASTER (
      ClienbtNo varchar(6) primary key,
     Name varchar(20),
@@ -22,8 +14,6 @@ create table CLIENT_MASTER (
     State varchar(15),
     Baldue int(10)
 );
-
-DROP table CLIENT_MASTER;
 
 insert into CLIENT_MASTER  
 values 
@@ -36,9 +26,11 @@ values
 
 select * from CLIENT_MASTER;
 
-    create table PRODUCT_MASTER (
+drop table  PRODUCT_MASTER;
 
-     ProductNo varchar(6) auto_increment primary key,
+create table PRODUCT_MASTER (
+
+     ProductNo varchar(6) primary key,
      Description varchar(15) NOT NULL,
      Profitpercent int(4) NOT NULL,
      Unitmesure varchar(10) NOT NULL,
@@ -63,8 +55,7 @@ values
 SELECT * FROM PRODUCT_MASTER;
 
 create table SALESMAN_MASTER (
-    
-    salesmanno varchar(6) auto_increment primary key,
+    salesmanno varchar(6)  primary key,
     salesmanName varchar(15) NOT NULL,
     Address1 varchar(4) NOT NULL,
     Address2 varchar(10),
@@ -76,15 +67,16 @@ create table SALESMAN_MASTER (
     Ytdsales int(6) NOT NULL,
     remarks varchar(60) NOT NULL
     );
-
-       insert into SALESMAN_MASTER  values 
+    
+    insert into SALESMAN_MASTER  values 
 ('S00001','Aman','a/14','worli','Mumbai',400002,'Maharashtra',30000,100,50,'Good'),
 ('S00002','Omkar','65','Nariman','Mumbai',400001,'Maharashtra',3000,200,100,'Good'),
 ('S00003','Raj','p-7','Bandra','Mumbai',400032,'Maharashtra',3000,200,100,'Good'),
 ('S00004','Ashish','A/5','Juhu','Mumbai',400044,'Maharashtra',3500,200,150,'Good');
 
     select * from SALESMAN_MASTER;
-
+    
+    
     CREATE TABLE SALES_ORDER (
 	ORDERNO varchar(6) primary key,
 	clientnumber varchar(6),
@@ -98,7 +90,7 @@ create table SALESMAN_MASTER (
 	FOREIGN  KEY (clientnumber) REFERENCES client_master(ClienbtNo),
     FOREIGN KEY (salesmannumber) REFERENCES SALESMAN_MASTER(salesmanno)
  );
-
+ 
  insert into SALES_ORDER (ORDERNO, clientnumber, orderddate, delyaddr, salesmannumber, delytype, billyn, delydate, orderstatus) values 
 ('O19001', 'C00001' , '2004-06-12','Address 1','S00001' , 'F' , 'N','2004-06-20' , 'In Process'),
 ('O19002', 'C00002' ,'2004-06-25','Address 2','S00002' , 'P' , 'N','2004-06-27' , 'Cancelled'),
@@ -109,35 +101,32 @@ create table SALESMAN_MASTER (
 
 select * from SALES_ORDER;
 
-
+drop  table sales_order_details;
 create table SALES_ORDER_DETAILS (
-ORDERNO varchar(6) ,
-PRODUCTNUMBER varchar(6),
-QTYORDERED int ,
-QTYDISP int,
-PRODUCTRATE int,
-
-	FOREIGN  KEY (clientnumber) REFERENCES client_master(ClienbtNo),
-    FOREIGN KEY (PRODUCTNUMBER) REFERENCES PRODUCT_MASTER(ProductNo)
+ORDER_ID varchar(6) ,
+PRODUCT_ID varchar(6),
+QUANTITY int ,
+DISCOUNT int,
+AMOUNT int,
+FOREIGN  KEY (ORDER_ID) REFERENCES client_master(ClienbtNo),
+FOREIGN KEY (PRODUCT_ID) REFERENCES PRODUCT_MASTER(ProductNo)
 );
 
-
-insert into SALES_ORDER_DETAILS VALUES 
-('O19001', 'P00001', 4 , 4 , 525),
-('O19001', 'P07965', 2 , 1 , 8400),
-('O19001', 'P07885', 2 , 1 , 5250),
-('O19002', 'P00001', 10 , 0 , 525),
-('O46865', 'P07868', 3 , 3 , 3150),
-('O46865', 'P07885', 3 , 1 , 5250),
-('O46865', 'P00001', 10 , 10 , 525),
-('O46865', 'P0345', 4 , 4 , 1050),
-('O19003', 'P00001', 2 , 2 , 1050),
-('O19003', 'P06734', 1 , 1 , 12000),
-('O46866', 'P07965', 1 , 0 , 8400),
-('O46866', 'P07975', 1, 0 , 1050),
-('O19008', 'P00001', 10 , 5 , 525),
-('O19008', 'P07975', 5 , 3 , 1050);
+	INSERT INTO SALES_ORDER_DETAILS (ORDER_ID, PRODUCT_ID, QUANTITY, DISCOUNT, AMOUNT) VALUES 
+('O19001', 'P00001', 4, 4, 525),
+('O19001', 'P07965', 2, 1, 8400),
+('O19001', 'P07885', 2, 1, 5250),
+('O19002', 'P00001', 10, 0, 525),
+('O46865', 'P07868', 3, 3, 3150),
+('O46865', 'P07885', 3, 1, 5250),
+('O46865', 'P00001', 10, 10, 525),
+('O46865', 'P0345', 4, 4, 1050),
+('O19003', 'P00001', 2, 2, 1050),
+('O19003', 'P06734', 1, 1, 12000),
+('O46766', 'P07965', 1, 0, 8400),
+('O46866', 'P07975', 1, 0, 1050),
+('O19008', 'P00001', 10, 5, 525),
+('O19008', 'P07975', 5, 3, 1050);
 
 
 select * from SALES_ORDER_DETAILS;
-
