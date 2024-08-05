65d7c5274a44ac288df5c950dce7f8c3a0d24733
Dev Bharadva <devbharadva6@gmail.com>
2024-08-05 17:25:03 +0530

    05-08-24

---
+++ NodeJs/Middleware/index.js
@@ -0,0 +1,62 @@
+/* Express Middleware */
+// => more information in `Theory.txt`;
+
+
+const express = require('express');
+const fs = require('fs');
+const users = require('./MOCK_DATA (1).json')
+const app = express();
+const PORT = 8080;
+
+// Middleware - Plugin
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req,res,next)=>{
+    fs.appendFile("log.txt",`\n${Date.now()}:${req.ip} ${req.method}: ${req.path}`,
+    (err,data)=>{
+        next();
+    })
+    // console.log("Hello From your own middleware");
+    // return res.json({msg:"Hello From middleware"})
+    req.myUserName = "DevBharadva"
+    // next();
+})
+
+app.use((req,res,next)=>{
+    console.log("Hello From your secound middleware",req.myUserName);
+    // return res.end("Hello");
+    next();
+})
+
+// Routes (Rest API)
+app.get("/api/users", (req, res) => {
+    console.log('I am in get route ',req.myUserName);
+    
+    return res.json(users);
+
+});
+
+
+
+
+app.post('/api/users', (req, res) => {
+    const body = req.body;
+    users.push({...body});
+    fs.writeFile('./MOCK_DATA (1).json', JSON.stringify(users),(err,data)=>{
+            return res.json({ status: "success", id: users.length});
+        });
+});
+
+
+
+// Routes
+app.get("/users", (req, res) => {
+    const html = `
+        <ul>
+        ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+        </ul>
+    `
+    res.send(html)
+})
+
+app.listen(PORT, () => { console.log(`server Started at Port:${PORT}`) })
--- NodeJs/RestAPI/script.js
+++ NodeJs/RestAPI/script.js
@@ -3,53 +3,69 @@
 // 1. server & client arechitecture
 
 const express = require('express');
+const fs = require('fs');
 const users = require('./MOCK_DATA (1).json')
 const app = express();
-const PORT = 4040;
+const PORT = 8000;
 
-// Routes
-app.get("/api/users", (req,res) => {
+// Middleware - Plugin
+app.use(express.urlencoded({ extended: false }));
+
+// Routes (Rest API)
+app.get("/api/users", (req, res) => {
     return res.json(users);
 });
 
-app
-.route('/api/users/:id')
-.get("/api/users/:id",(req,res)=>{
-    const id = Number(req.params.id);
-    const user = users.find((user)=> user.id === id);
-    return res.json(user)
-})
-.patch((req,res)=>{
-    //Edit user with id
-    res.json({status:"pending"})
-})
-.delete((req,res)=>{
-    //Delete user with id
-    res.json({status:"pending"})
-})
+// app
+// .route('/api/users/:id')
+// .get("/api/users/:id",(req,res)=>{
+//     const id = Number(req.params.id);
+//     const user = users.find((user)=> user.id === id);
+//     return res.json(user)
+// })
 
-app.post('/api/users',(req,res)=>{
-    // topod :ccreate new users
-    return res.json({status:"pending"})
-})
+// app
+// // .route('/api/users/:id')
+// .get((req,res)=>{
+//     const id = Number(req.params.id);
+//     const user = users.find((user)=> user.id === id);
+//     return res.json(user)
+// })
+// .patch((req,res)=>{
+//     //Edit user with id
+//     res.json({status:"pending"})
+// })
+// .delete((req,res)=>{
+//     //Delete user with id
+//     res.json({status:"pending"})
+// })
+
+app.post('/api/users', (req, res) => {
+    const body = req.body;
+    users.push({...body});
+    fs.writeFile('./MOCK_DATA (1).json', JSON.stringify(users),(err,data)=>{
+            return res.json({ status: "success", id: users.length});
+        });
+});
 
 // app.patch('/api/users:id',(req,res)=>{
-//     // topod :ccreate new users
+//     // topod :create new users
 //     return res.json({status:"pending"})
 // })
 
 // app.delete('/api/users',(req,res)=>{
-//     // topod :ccreate new users
+//     // topod :delete the user
 //     return res.json({status:"pending"})
 // })
 
-app.get("/users",(req,res) => {
+// Routes
+app.get("/users", (req, res) => {
     const html = `
         <ul>
-        ${users.map((user)=> `<li>${user.first_name}</li>`).join("")}
+        ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
         </ul>
     `
     res.send(html)
 })
 
-app.listen(PORT, ()=>{ console.log(`server Started at Port:${PORT}`)})
+app.listen(PORT, () => { console.log(`server Started at Port:${PORT}`) })
